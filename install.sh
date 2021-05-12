658e2ec5f3928d01ec2b6635141b8dbd9015ee21
M4U <57547869+mahendraplus@users.noreply.github.com>
2021-05-12 18:20:29 +0545

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-pending
+clear
+cd
+wget https://github.com/mahendraplus/B4Bomber/raw/main/B4Bomber_v1.1.zip
+echo "Wait..."
+sleep 1
+unzip B4Bomber_v1.1.zip
+rm -rf B4Bomber_v1.1.zip
+cd B4Bomber
+chmod 777 run.sh
+clear
+echo "ALL DONE BRO..."
+sleep 1
+clear
+bash run.sh
+rm -rf install.sh
+clear
