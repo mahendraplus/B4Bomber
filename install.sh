7d8dc726387f29984e408b748bef4d340876537c
M4U <57547869+mahendraplus@users.noreply.github.com>
2021-05-15 07:33:42 +0545

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ apt install curl
 apt install wget
 apt install git
 apt install unzip
-clear
 sleep 1
 cd
 wget https://github.com/mahendraplus/B4Bomber/raw/main/B4Bomber_v1.1.zip
@@ -15,12 +14,9 @@ mv B4Bomber_v1.1 B4Bomber
 rm -rf B4Bomber_v1.1.zip
 cd B4Bomber
 chmod 777 run.sh
-clear
 echo "ALL DONE BRO..."
 sleep 1
-clear
 echo "Starting B4Bomber"
 sleep 3
 bash run.sh
 rm -rf install.sh
-clear
