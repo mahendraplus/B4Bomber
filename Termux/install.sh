c832e0d91f025b6cca749a6f47b26167dc6426fa
M4U <57547869+mahendraplus@users.noreply.github.com>
2021-06-01 23:07:57 +0530

    Update install.sh

--- Termux/install.sh
+++ Termux/install.sh
@@ -2,7 +2,8 @@
 cd
 apt install git 
 git clone https://github.com/mahendraplus/B4Bomber
-mv /data/data/com.termux/files/home/B4Bomber/Termux/b4b /data/data/com.termux/files/usr/bin
+chmod 777 B4Bomber/Termux/b4bomber
+mv B4Bomber/Termux/b4b /data/data/com.termux/files/usr/bin
 chmod 777 /data/data/com.termux/files/usr/bin/b4b
 echo "Execute By Typing b4b"
 b4b
