7ebd40506567d060d35ccdc6ac55ce6f923cd27f
ᨀ Max ᨀ <57547869+mahendraplus@users.noreply.github.com>
2022-11-22 22:22:07 +0545

    Update start.sh

--- Suck/start.sh
+++ Suck/start.sh
@@ -1,5 +1,4 @@
 mkdir Max
 cd Max
 wget https://raw.githubusercontent.com/mahendraplus/B4Bomber/main/Suck/m4u.xt
-wget https://raw.githubusercontent.com/mahendraplus/B4Bomber/main/Suck/max.sh
 bash max.sh
